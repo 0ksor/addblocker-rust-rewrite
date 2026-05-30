5b20758c0a2e276cc45551874e4e954043391a51
0ksorn <oguzaktas04@hotmail.com>
2026-05-30 14:33:41 +0200

    zbus is working omg oh yes Pog

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,18 @@
 use tokio::time::{Duration, sleep};
 use zbus::Connection;
 
-async fn play_pause(conn: &Connection, command: Command) -> zbus::Result<()> {
-    let result = conn
-        .call_method(
-            Some("org.mpris.MediaPlayer2.spotify"),
-            "/org/mpris/MediaPlayer2",
-            Some("org.mpris.MediaPlayer2.Player"),
-            command.as_method(),
-            &(),
-        )
-        .await;
-
-    match result {
-        Ok(_) => {
-            println!("{} OK", command.as_method());
-            Ok(())
-        }
-        Err(e) => {
-            println!("{} failed: {e}", command.as_method());
-            Err(e)
-        }
-    }
-}
-
-enum Command {
-    Play,
-    Pause,
-}
-impl Command {
-    fn as_method(&self) -> &'static str {
-        match self {
-            Self::Play => "Play",
-            Self::Pause => "Pause",
-        }
-    }
-}
+use spotify::SpotifyPlayerProxy;
+mod spotify;
 
 #[tokio::main]
 async fn main() -> zbus::Result<()> {
     let conn = Connection::session().await?;
-    play_pause(&conn, Command::Play).await?;
-    sleep(Duration::from_secs(5)).await;
-    play_pause(&conn, Command::Pause).await?;
+    let spotfy = SpotifyPlayerProxy::new(&conn).await?;
+    let st = spotfy.playback_status().await.unwrap();
+    spotfy.play().await.unwrap();
+    sleep(Duration::from_millis(20)).await;
+    let sta = spotfy.playback_status().await.unwrap();
+    println!("{st}\n{sta}");
 
     Ok(())
 }
---
+++ src/spotify.rs
@@ -0,0 +1,22 @@
+use zbus::proxy;
+
+#[proxy(
+    interface = "org.mpris.MediaPlayer2.Player",
+    default_service = "org.mpris.MediaPlayer2.spotify",
+    default_path = "/org/mpris/MediaPlayer2"
+)]
+trait SpotifyPlayer {
+    fn play(&self) -> zbus::Result<()>;
+    fn pause(&self) -> zbus::Result<()>;
+    fn play_pause(&self) -> zbus::Result<()>;
+    fn next(&self) -> zbus::Result<()>;
+    fn previous(&self) -> zbus::Result<()>;
+
+    #[zbus(property)]
+    fn playback_status(&self) -> zbus::Result<String>;
+
+    #[zbus(property)]
+    fn metadata(
+        &self,
+    ) -> zbus::Result<std::collections::HashMap<String, zbus::zvariant::OwnedValue>>;
+}
