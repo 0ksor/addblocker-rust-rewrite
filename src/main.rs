f2889d3867d499e23a35351737ebb960f6ca35b2
0ksorn <oguzaktas04@hotmail.com>
2026-05-30 22:55:36 +0200

    geting artist and learning how to work around types of dbus/zbus

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio::time::{Duration, sleep};
+// use tokio::time::{Duration, sleep};
 use zbus::Connection;
 
 use spotify::SpotifyPlayerProxy;
@@ -9,12 +9,21 @@ async fn main() -> zbus::Result<()> {
     let conn = Connection::session().await?;
     let spotfy = SpotifyPlayerProxy::new(&conn).await?;
 
-    if let Err(e) = spotfy.playback_status().await {
-        print!("Error: {e}");
+    match spotfy.playback_status().await {
+        Err(e) => {
+            eprintln!("Error: {e}");
+            return Ok(());
+        }
+        Ok(s) => println!("{s}"),
     }
-    if let Err(e) = spotfy.play().await {
-        eprintln!("Error: {e}");
-    }
+    // if let Err(e) = spotfy.play().await {
+    //     eprintln!("Error: {e}");
+    // }
+    let mut a = spotfy.metadata().await.unwrap();
+    let artists: Vec<String> = a.remove("xesam:artist").unwrap().try_into().unwrap();
+    // .value_signature()
+    // .to_string();
+    println!("{}", artists[0]);
 
     Ok(())
 }
