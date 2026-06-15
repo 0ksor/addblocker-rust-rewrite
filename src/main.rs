e614d4e715a363e94cafe2e3976ef9509ccf88fa
0ksorn <oguzaktas04@hotmail.com>
2026-06-15 16:02:14 +0200

    polling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::future::poll_fn;
+use std::pin::pin;
 use std::process::Command;
+use std::process::Stdio;
+
+use futures_core::stream::Stream;
 use tokio::time::{Duration, sleep};
 use zbus::Connection;
 
@@ -8,24 +13,27 @@ use spotify::SpotifyRootProxy;
 
 #[tokio::main]
 async fn main() -> zbus::Result<()> {
-    Command::new("spotify").spawn().ok();
-    sleep(Duration::from_secs(4)).await;
-
+    Command::new("spotify")
+        .stderr(Stdio::null())
+        .stdout(Stdio::null())
+        .spawn()
+        .ok();
     let conn = Connection::session().await?;
     // wait until spotify launches
     // then create the proxys
     let (spotify, root) = loop {
-        if let Ok(s) = SpotifyPlayerProxy::new(&conn).await {
-            let root = SpotifyRootProxy::new(&conn).await?;
-            break (s, root);
-        } else {
-            sleep(Duration::from_millis(200)).await;
+        if let Ok(s) = SpotifyPlayerProxy::new(&conn).await
+            && let Ok(r) = SpotifyRootProxy::new(&conn).await
+        {
+            break (s, r);
         }
+        sleep(Duration::from_millis(200)).await;
     };
+    let mut changes = pin!(spotify.receive_metadata_changed().await);
+
     loop {
+        poll_fn(|cx| changes.as_mut().poll_next(cx)).await;
         log(&spotify).await;
-        sleep(Duration::from_secs(4)).await;
-        root.quit().await?;
     }
 }
 
@@ -35,7 +43,7 @@ async fn log(spotify: &SpotifyPlayerProxy<'_>) {
     let album: String = meta.remove("xesam:album").unwrap().try_into().unwrap();
     let title: String = meta.remove("xesam:title").unwrap().try_into().unwrap();
     println!(
-        "artist: {}\n Album: {}\n title: {}",
+        "artist: {}\n Album: {}\n title: {}\n",
         artists.join(""),
         album,
         title,
